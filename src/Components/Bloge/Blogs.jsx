6502c9a6b13052e97667e05a8fee1728e17db674
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2025-05-09 17:51:16 +0330

    layout blogs and create card component

--- src/Components/Bloge/Blogs.jsx
+++ src/Components/Bloge/Blogs.jsx
@@ -1,14 +1,25 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
 import { GET_BLOGS_INFO } from '../../graphql/queries'
+import { Grid } from '@mui/material';
+import CardEL from '../Shared/CardEL';
 
 function Blogs() {
-  const{loading, errors,data} = useQuery(GET_BLOGS_INFO);
+  const{loading,errors,data} = useQuery(GET_BLOGS_INFO);
   if(loading) return <h2>Loading...</h2>
   if(errors) return <h2>error...</h2>
-  console.log(data);
+ 
   return (
-    <div>Blogs</div>
+    <>
+    <Grid container spacing={2}>
+    {data.posts.map(post=>(
+      <Grid size={{xs: 12, sm:6 ,md:4}} key={post.id}  >
+       <CardEL {...post}  />
+      </Grid>
+    ))}
+
+    </Grid>
+    </>
   )
 }
 
---
+++ src/Components/Shared/CardEL.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function CardEL(props) {
+    
+  return (
+    <div>CardEL</div>
+  )
+}
+
+export default CardEL
