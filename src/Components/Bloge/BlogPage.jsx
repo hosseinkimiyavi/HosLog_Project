93b149e0fc05dfb0c582da2c9928fa622ca008fd
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2025-05-17 20:52:17 +0330

    add blogPage's details

--- src/Components/Bloge/BlogPage.jsx
+++ src/Components/Bloge/BlogPage.jsx
@@ -1,10 +1,14 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { GET_POST_INFO } from "../../graphql/queries";
 import Loader from "../Shared/Loader";
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import sanitizeHtml from "sanitize-html"
 
 export default function BlogPage() {
+  const Navigate = useNavigate(); //برای برگشتن به مرحله قبل
   const { slug } = useParams();
   const { error, loading, data } = useQuery(GET_POST_INFO, {
     variables: { slug },
@@ -13,8 +17,71 @@ export default function BlogPage() {
   if (error) return <h2>error...</h2>;
   console.log(data);
   const {
-    post: { author, content, coverPhoto, title },
+    post: {
+      author: { avatar, name, field },
+      content,
+      coverPhoto,
+      title,
+    },
   } = data;
-  
-  return <div>BlogPage</div>;
+
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Grid container mt={5}>
+          <Grid
+            size={{ xs: 12 }}
+            mt={5}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography component="h3" variant="h4" fontWeight={600}>
+              {title}
+            </Typography>
+            <ArrowBackRoundedIcon
+              color="action"
+              onClick={() => Navigate(-1)}
+              style={{ cursor: "pointer" }}
+            />
+          </Grid>
+          <Grid size={{ xs: 12 }} mt={6}>
+            <img
+              src={coverPhoto.url}
+              alt={title}
+              width="100%"
+              style={{ borderRadius: 15 }}
+            />
+          </Grid>
+          <Grid size={{ xs: 12 }} mt={2} display="flex" alignItems="center">
+            <Avatar
+              src={avatar.url}
+              sx={{ width: 80, height: 80, marginRight: 2 }}
+            />
+            <Box component="div">
+              <Typography component="h4" variant="h5" fontWeight={600}>
+                {name}
+              </Typography>
+              <Typography
+                component="h6"
+                variant="h6"
+                fontWeight={600}
+                color="textSecondary"
+              >
+                {field}
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid size={{ xs: 12 }} mt={4}>
+            <div
+              className="  text-2xl   break-words w-full whitespace-pre-wrap [&_pre]:whitespace-pre-wrap [&_code]:break-words [&_pre]:overflow-x-auto "
+              dangerouslySetInnerHTML={{
+                __html: sanitizeHtml(content.html),
+              }}
+            ></div>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
