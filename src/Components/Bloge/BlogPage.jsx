4fa9a6a7f06a169fa106afaef9e44eeb6688fe40
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2025-05-17 18:38:32 +0330

    write query for get postData

--- src/Components/Bloge/BlogPage.jsx
+++ src/Components/Bloge/BlogPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { GET_POST_INFO } from "../../graphql/queries";
+import Loader from "../Shared/Loader";
 
 export default function BlogPage() {
-  return (
-    <div>BlogPage</div>
-  )
+  const { slug } = useParams();
+  const { error, loading, data } = useQuery(GET_POST_INFO, {
+    variables: { slug },
+  });
+  if (loading) return <Loader />;
+  if (error) return <h2>error...</h2>;
+  console.log(data);
+  const {
+    post: { author, content, coverPhoto, title },
+  } = data;
+  
+  return <div>BlogPage</div>;
 }
